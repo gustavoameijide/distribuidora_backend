ac63f46444f0e760a22df7b6312504c59b6a947b
martincamer <martincamer011@gmail.com>
2024-04-14 20:22:15 -0300

    asd

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,19 +1,8 @@
 import jwt from "jsonwebtoken";
 
-/*export const createAccessToken = (payload) => {
-  return new Promise((resolve, reject) => {
-    jwt.sign(payload, "react2021", (err, token) => {
-      if (err) reject(err);
-      resolve(token);
-    });
-  });
-};*/
-
 export const createAccessToken = (payload) => {
   return new Promise((resolve, reject) => {
-    const expiresIn = "10d"; // Set the expiration time to 10 days
-
-    jwt.sign(payload, "react2021", { expiresIn }, (err, token) => {
+    jwt.sign(payload, "react2021", { expiresIn: "1d" }, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
